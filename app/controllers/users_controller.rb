8a73fb5b53de02d39dacf23eea1c9243657dda8b
neener <janinemrosen@gmail.com>
2014-10-29 21:57:51 -0400

    new users savings to db

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
 
  def create
  	user = User.create(email: params["email"], password: params["password"])
- 	render(:index)
+ 	redirect_to '/'
  end
 
  def show
